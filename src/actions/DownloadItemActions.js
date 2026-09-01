c2e4d5073cb6917312ac384d0aa5e1991febb564
Jørgen Berntsen <berntsen.jorgen@gmail.com>
2026-09-01 16:20:37 +0200

    Use cookie that can be retrieved

--- src/actions/DownloadItemActions.js
+++ src/actions/DownloadItemActions.js
@@ -127,7 +127,7 @@ export const autoAddItemFromLocalStorage = () => (dispatch, getState) => {
         localStorage.autoAddDownloadItemOnLoad !== "undefined" &&
         localStorage.autoAddDownloadItemOnLoad !== "null";
     const itemToAdd = hasItemToAdd ? JSON.parse(localStorage.autoAddDownloadItemOnLoad) : null;
-    const isLoggedIn = getCookie("BearerToken").length > 0;
+    const isLoggedIn = getCookie("oidcAccessToken").length > 0;
     //const hasBaatInfo = getState().baatInfo && getState().baatInfo.user;
     if (itemToAdd && isLoggedIn /* && hasBaatInfo */) { //TODO
         dispatch(addItemSelectedForDownload(itemToAdd));
